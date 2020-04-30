51f1ff4aa59981c80297279b7f770837a03d245c
Ivan Lorna <ivanlorna1710@gmail.com>
2020-04-30 10:37:28 -0700

    created working base controller, camera operable

---
+++ controllers/my_controller_c/build/release/my_controller_c.d
@@ -0,0 +1,6 @@
+build/release/my_controller_c.o: my_controller_c.c \
+ E:\Programs\Webots/include/controller/c/webots/robot.h \
+ E:\Programs\Webots/include/controller/c/webots/types.h \
+ E:\Programs\Webots/include/controller/c/webots/nodes.h \
+ E:\Programs\Webots/include/controller/c/webots/camera.h \
+ E:\Programs\Webots/include/controller/c/webots/camera_recognition_object.h
