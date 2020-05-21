748407754bec4986c42dc948093ec391f55b5338
ehalf001 <38255772+ehalf001@users.noreply.github.com>
2020-05-21 11:43:47 -0700

    Fixing issues when merging.
    Removing old files from master after merge.

--- mantis/controllers/mantis/build/release/mantis.d
+++
@@ -1,6 +0,0 @@
-build/release/mantis.o: mantis.c \
- C:\Users\User\AppData\Local\Programs\Webots/include/controller/c/webots/motor.h \
- C:\Users\User\AppData\Local\Programs\Webots/include/controller/c/webots/types.h \
- C:\Users\User\AppData\Local\Programs\Webots/include/controller/c/webots/robot.h \
- C:\Users\User\AppData\Local\Programs\Webots/include/controller/c/webots/nodes.h \
- C:\Users\User\AppData\Local\Programs\Webots/include/controller/c/webots/Keyboard.h
