112b8bc448fcf654f1c7a483bc546a6e26bda9e2
clega001 <clega001@ucr.edu>
2020-05-22 15:08:02 -0700

    display path

---
+++ controllers/my_controller_c/Auxillaries/PathDisplay.c
@@ -0,0 +1,51 @@
+#include <webots/robot.h>
+#include <webots/gps.h>
+#include <webots/display.h>
+
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define GROUND_X 1.0
+#define GROUND_Z 1.0
+
+#define white 0xFFFFFF
+#define black 0x000000
+#define blue 0x2222BB
+
+struct PathDisplay{
+  WbDeviceTag Hexabot_PathDisplay;
+  int height, width;
+};
+
+struct PathDisplay PathDisplay_Init() {
+  struct PathDisplay PathDisplayInit;
+
+  PathDisplayInit.Hexabot_PathDisplay = wb_robot_get_device("Path_Display");
+  
+  PathDisplayInit.width = wb_display_get_width(PathDisplayInit.Hexabot_PathDisplay);
+  PathDisplayInit.height = wb_display_get_height(PathDisplayInit.Hexabot_PathDisplay);
+  
+  wb_display_set_color(PathDisplayInit.Hexabot_PathDisplay, white);
+  wb_display_fill_rectangle(PathDisplayInit.Hexabot_PathDisplay, 0, 0, PathDisplayInit.width, PathDisplayInit.height);
+  wb_display_set_color(PathDisplayInit.Hexabot_PathDisplay, black);
+  wb_display_draw_line(PathDisplayInit.Hexabot_PathDisplay, 0, PathDisplayInit.height / 2, PathDisplayInit.width - 1, PathDisplayInit.height / 2);
+  wb_display_draw_text(PathDisplayInit.Hexabot_PathDisplay, "x", PathDisplayInit.width - 10, PathDisplayInit.height / 2 - 10);
+  wb_display_draw_line(PathDisplayInit.Hexabot_PathDisplay, PathDisplayInit.width / 2, 0, PathDisplayInit.width / 2, PathDisplayInit.height - 1);
+  wb_display_draw_text(PathDisplayInit.Hexabot_PathDisplay, "z", PathDisplayInit.width / 2 - 10, PathDisplayInit.height - 10);
+
+  return PathDisplayInit;
+}
+
+struct PathDisplay PathDisplay_Loop(struct PathDisplay PathDisplayOld, struct GPS gps) {
+
+    struct PathDisplay PathDisplayNew = PathDisplayOld;
+    // display the robot position
+    wb_display_set_opacity(PathDisplayNew.Hexabot_PathDisplay, 0.03);
+    wb_display_set_color(PathDisplayNew.Hexabot_PathDisplay, blue);
+    wb_display_fill_oval(PathDisplayNew.Hexabot_PathDisplay, PathDisplayNew.width * (gps.pos_x + GROUND_X / 2) / GROUND_X,
+                         PathDisplayNew.height * (gps.pos_z + GROUND_Z / 2) / GROUND_Z, 4, 4);
+
+    return PathDisplayNew;
+}
--- controllers/my_controller_c/build/release/my_controller_c.d
+++ controllers/my_controller_c/build/release/my_controller_c.d
@@ -2,14 +2,18 @@ build/release/my_controller_c.o: my_controller_c.c \
  C:\Users\Christian\ Legaspino\AppData\Local\Programs\Webots/include/controller/c/webots/robot.h \
  C:\Users\Christian\ Legaspino\AppData\Local\Programs\Webots/include/controller/c/webots/types.h \
  C:\Users\Christian\ Legaspino\AppData\Local\Programs\Webots/include/controller/c/webots/nodes.h \
- C:\Users\Christian\ Legaspino\AppData\Local\Programs\Webots/include/controller/c/webots/camera.h \
- C:\Users\Christian\ Legaspino\AppData\Local\Programs\Webots/include/controller/c/webots/camera_recognition_object.h \
  C:\Users\Christian\ Legaspino\AppData\Local\Programs\Webots/include/controller/c/webots/motor.h \
  C:\Users\Christian\ Legaspino\AppData\Local\Programs\Webots/include/controller/c/webots/Keyboard.h \
- C:\Users\Christian\ Legaspino\AppData\Local\Programs\Webots/include/controller/c/webots/gps.h \
  C:\Users\Christian\ Legaspino\AppData\Local\Programs\Webots/include/controller/c/webots/gyro.h \
+ Auxillaries/GPS.c \
+ C:\Users\Christian\ Legaspino\AppData\Local\Programs\Webots/include/controller/c/webots/gps.h \
+ Auxillaries/Compass.c \
  C:\Users\Christian\ Legaspino\AppData\Local\Programs\Webots/include/controller/c/webots/compass.h \
- C:\Users\Christian\ Legaspino\AppData\Local\Programs\Webots/include/controller/c/webots/display.h \
+ Auxillaries/Camera.c \
+ C:\Users\Christian\ Legaspino\AppData\Local\Programs\Webots/include/controller/c/webots/camera.h \
+ C:\Users\Christian\ Legaspino\AppData\Local\Programs\Webots/include/controller/c/webots/camera_recognition_object.h \
+ Auxillaries/Lidar.c \
  C:\Users\Christian\ Legaspino\AppData\Local\Programs\Webots/include/controller/c/webots/lidar.h \
  C:\Users\Christian\ Legaspino\AppData\Local\Programs\Webots/include/controller/c/webots/lidar_point.h \
- C:\Users\Christian\ Legaspino\AppData\Local\Programs\Webots/include/controller/c/webots/camera_recognition_object.h
+ C:\Users\Christian\ Legaspino\AppData\Local\Programs\Webots/include/controller/c/webots/display.h \
+ Auxillaries/PathDisplay.c
