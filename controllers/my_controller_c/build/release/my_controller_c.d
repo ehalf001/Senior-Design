6c24f49a104e3ccf9b7cfa4fb6ba431293e532b5
ehalf001 <38255772+ehalf001@users.noreply.github.com>
2020-05-22 16:46:41 -0700

    Added Walking File
    Added a walking File that contains turn left, turn right, walk foward and backwards

--- controllers/my_controller_c/build/release/my_controller_c.d
+++ controllers/my_controller_c/build/release/my_controller_c.d
@@ -7,10 +7,12 @@ build/release/my_controller_c.o: my_controller_c.c \
  C:\Users\User\AppData\Local\Programs\Webots/include/controller/c/webots/motor.h \
  C:\Users\User\AppData\Local\Programs\Webots/include/controller/c/webots/Keyboard.h \
  C:\Users\User\AppData\Local\Programs\Webots/include/controller/c/webots/gyro.h \
- C:\Users\User\AppData\Local\Programs\Webots/include/controller/c/webots/compass.h \
  C:\Users\User\AppData\Local\Programs\Webots/include/controller/c/webots/display.h \
  C:\Users\User\AppData\Local\Programs\Webots/include/controller/c/webots/lidar.h \
  C:\Users\User\AppData\Local\Programs\Webots/include/controller/c/webots/lidar_point.h \
  C:\Users\User\AppData\Local\Programs\Webots/include/controller/c/webots/camera_recognition_object.h \
  Auxillaries/GPS.c \
- C:\Users\User\AppData\Local\Programs\Webots/include/controller/c/webots/gps.h
+ C:\Users\User\AppData\Local\Programs\Webots/include/controller/c/webots/gps.h \
+ Auxillaries/Compass.c \
+ C:\Users\User\AppData\Local\Programs\Webots/include/controller/c/webots/compass.h \
+ System/Walking.c
--- controllers/my_controller_c/my_controller_c.c
+++ controllers/my_controller_c/my_controller_c.c
@@ -1,3 +1,4 @@
+
 /*
  * File:          my_controller_c.c
  * Date:
@@ -27,6 +28,7 @@
 
 #include "Auxillaries/GPS.c"
 #include "Auxillaries/Compass.c"
+#include "System/Walking.c"
 
 #define purple 0xC030C0
 #define black 0x000000
@@ -78,7 +80,7 @@ int main(int argc, char **argv) {
 
    WbDeviceTag Hexabot_Camera = wb_robot_get_device("Hexabot_Camera");
    wb_camera_enable(Hexabot_Camera, 10);
-     wb_camera_recognition_enable(Hexabot_Camera, TIME_STEP);
+   wb_camera_recognition_enable(Hexabot_Camera, TIME_STEP);
    
     // get current number of object recognized by the camera
     int numObjects = wb_camera_recognition_get_number_of_objects(Hexabot_Camera);
@@ -100,6 +102,7 @@ int main(int argc, char **argv) {
                objects[i].colors[3 * j + 1], objects[i].colors[3 * j + 2]);
     }
 
+
   WbDeviceTag utm30lx, Lidar_Display;
   // enable hokuyo lidar
   utm30lx = wb_robot_get_device("Hokuyo UTM-30LX");
@@ -119,36 +122,13 @@ int main(int argc, char **argv) {
 
    //GPS Enable
    struct GPS Gps = GPS_Init();
-   
-    //Gyro enable
-   WbDeviceTag Hexabot_Gyro = wb_robot_get_device("Hexabot_Gyro");
-   wb_gyro_enable(Hexabot_Gyro,10);
 
-   //Compass Engable
+   //Compass Enable
    struct Compass COMP = Compass_Init();
    
-   //Robot Walking Motors
-   WbDeviceTag Hexabot_Motors[18] = {wb_robot_get_device("Hexabot_Leg0_Motor1"), wb_robot_get_device("Hexabot_Leg0_Motor2"), wb_robot_get_device("Hexabot_Leg0_Motor3"),
-                                    wb_robot_get_device("Hexabot_Leg1_Motor1"), wb_robot_get_device("Hexabot_Leg1_Motor2"), wb_robot_get_device("Hexabot_Leg1_Motor3"),
-                                    wb_robot_get_device("Hexabot_Leg2_Motor1"), wb_robot_get_device("Hexabot_Leg2_Motor2"), wb_robot_get_device("Hexabot_Leg2_Motor3"), 
-                                    wb_robot_get_device("Hexabot_Leg3_Motor1"), wb_robot_get_device("Hexabot_Leg3_Motor2"), wb_robot_get_device("Hexabot_Leg3_Motor3"),
-                                    wb_robot_get_device("Hexabot_Leg4_Motor1"), wb_robot_get_device("Hexabot_Leg4_Motor2"), wb_robot_get_device("Hexabot_Leg4_Motor3"),
-                                    wb_robot_get_device("Hexabot_Leg5_Motor1"), wb_robot_get_device("Hexabot_Leg5_Motor2"), wb_robot_get_device("Hexabot_Leg5_Motor3")
-                                     };
-   
-   //Robot Walking Variables
-   const double f = .5;
-   const double a[18] = {.1, .1, -.1, -.1, -.1, .1, .1, .1, -.1, .1, .1, -.1, -.1, -.1, .1, .1, .1, -.1};
-   const double p[18] = {0, 2, 2.5, 0, 2, 2.5, 0, 2, 2.5, 0, 2, 2.5, 0, 2, 2.5, 0, 2, 2.5};
-   const double d[18] = {-.1, -.2, -.3, .5, -.2, -.3, 1.1, -.2, -.3, -.1, -.2, -.3, 0, -.2, .1, 0, -.2, .1};
-   //Enable Walking
-   wb_keyboard_enable(TIME_STEP);
-   int key;
-   
-   int i;//for loop
-   for (i = 0; i < 18; ++i)  // Apply a sinuosidal function for each motor.
-           wb_motor_set_position(Hexabot_Motors[i], a[i] * sin(2.0 * M_PI * f * 1 + p[i]) + d[i]);
-   
+   //Walking Enable
+   struct Walking WalkPatt = Walking_Init();
+
    while (wb_robot_step(TIME_STEP) != -1) 
    {
       // lidar display  
@@ -156,29 +136,7 @@ int main(int argc, char **argv) {
       Display(Lidar_Display, display_width, display_height, utm30lx_values, utm30lx_samples, utm30lx_field_of_view);    
    
       double time = wb_robot_get_time();
-      key = wb_keyboard_get_key();
-      if(key == WB_KEYBOARD_UP)
-        for (i = 0; i < 18; ++i)  // Apply a sinuosidal function for each motor.
-           wb_motor_set_position(Hexabot_Motors[i], a[i] * sin(2.0 * M_PI * f * time + p[i]) + d[i]);
-      else if(key == WB_KEYBOARD_DOWN)
-        for (i = 0; i < 18; ++i)  // Apply a sinuosidal function for each motor.
-           wb_motor_set_position(Hexabot_Motors[i], a[i] * sin(-2.0 * M_PI * f * time + p[i]) + d[i]);
-      else if(key == WB_KEYBOARD_LEFT)
-      {
-        for (i = 0; i < 9; ++i)  // Apply a sinuosidal function for each motor.
-           wb_motor_set_position(Hexabot_Motors[i], a[i] * sin(2.0 * M_PI * f * time + p[i]) + d[i]);
-        for (i = 9; i < 18; ++i)  // Apply a sinuosidal function for each motor.
-           wb_motor_set_position(Hexabot_Motors[i], a[i] * sin(-2.0 * M_PI * f * time + p[i]) + d[i]);
-  
-      }
-      else if(key == WB_KEYBOARD_RIGHT)
-      {
-        for (i = 0; i < 9; ++i)  // Apply a sinuosidal function for each motor.
-           wb_motor_set_position(Hexabot_Motors[i], a[i] * sin(-2.0 * M_PI * f * time + p[i]) + d[i]);
-        for (i = 9; i < 18; ++i)  // Apply a sinuosidal function for each motor.
-           wb_motor_set_position(Hexabot_Motors[i], a[i] * sin(2.0 * M_PI * f * time + p[i]) + d[i]);
-        
-      }
+
 
       Gps = GPS_Loop(Gps);
       COMP = Compass_Loop(COMP);
@@ -186,94 +144,42 @@ int main(int argc, char **argv) {
       {
          Gps.angle = 90 - Gps.angle;
          if(COMP.degree > Gps.angle && abs(COMP.degree - Gps.angle) > 2)
-         {
-           for (i = 0; i < 9; ++i)  // Apply a sinuosidal function for each motor.
-             wb_motor_set_position(Hexabot_Motors[i], a[i] * sin(2.0 * M_PI * f * time + p[i]) + d[i]);
-           for (i = 9; i < 18; ++i)  // Apply a sinuosidal function for each motor.
-             wb_motor_set_position(Hexabot_Motors[i], a[i] * sin(-2.0 * M_PI * f * time + p[i]) + d[i]);
-         }
+           WalkPatt = TurnLEFT(WalkPatt, time);
          else if(COMP.degree < Gps.angle && abs(COMP.degree - Gps.angle) > 2)
-         {
-           for (i = 0; i < 9; ++i)  // Apply a sinuosidal function for each motor.
-             wb_motor_set_position(Hexabot_Motors[i], a[i] * sin(-2.0 * M_PI * f * time + p[i]) + d[i]);
-           for (i = 9; i < 18; ++i)  // Apply a sinuosidal function for each motor.
-             wb_motor_set_position(Hexabot_Motors[i], a[i] * sin(2.0 * M_PI * f * time + p[i]) + d[i]);
-         }
+            WalkPatt = TurnRIGHT(WalkPatt, time);
          else if(Gps.distance > .5)
-         {
-           for (i = 0; i < 18; ++i)  // Apply a sinuosidal function for each motor.
-             wb_motor_set_position(Hexabot_Motors[i], a[i] * sin(2.0 * M_PI * f * time + p[i]) + d[i]);
-         }
+            WalkPatt = Foward(WalkPatt, time);
          
        }
        else if(Gps.quadrant == 2)
        {
          Gps.angle = -90 - Gps.angle;
          if(COMP.degree > Gps.angle && abs(COMP.degree - Gps.angle) > 2)
-         {
-           for (i = 0; i < 9; ++i)  // Apply a sinuosidal function for each motor.
-             wb_motor_set_position(Hexabot_Motors[i], a[i] * sin(2.0 * M_PI * f * time + p[i]) + d[i]);
-           for (i = 9; i < 18; ++i)  // Apply a sinuosidal function for each motor.
-             wb_motor_set_position(Hexabot_Motors[i], a[i] * sin(-2.0 * M_PI * f * time + p[i]) + d[i]);
-         }
+            WalkPatt = TurnLEFT(WalkPatt, time);
          else if(COMP.degree < Gps.angle && abs(COMP.degree - Gps.angle) > 2)
-         {
-           for (i = 0; i < 9; ++i)  // Apply a sinuosidal function for each motor.
-             wb_motor_set_position(Hexabot_Motors[i], a[i] * sin(-2.0 * M_PI * f * time + p[i]) + d[i]);
-           for (i = 9; i < 18; ++i)  // Apply a sinuosidal function for each motor.
-             wb_motor_set_position(Hexabot_Motors[i], a[i] * sin(2.0 * M_PI * f * time + p[i]) + d[i]);
-         }
+            WalkPatt = TurnRIGHT(WalkPatt, time);
          else if(Gps.distance > .5)
-         {
-           for (i = 0; i < 18; ++i)  // Apply a sinuosidal function for each motor.
-             wb_motor_set_position(Hexabot_Motors[i], a[i] * sin(2.0 * M_PI * f * time + p[i]) + d[i]);
-         }
+            WalkPatt = Foward(WalkPatt, time);
        }
        else if(Gps.quadrant == 3)
        {
          Gps.angle = -90 - Gps.angle;
          if(COMP.degree > Gps.angle && abs(COMP.degree - Gps.angle) > 2)
-         {
-           for (i = 0; i < 9; ++i)  // Apply a sinuosidal function for each motor.
-             wb_motor_set_position(Hexabot_Motors[i], a[i] * sin(2.0 * M_PI * f * time + p[i]) + d[i]);
-           for (i = 9; i < 18; ++i)  // Apply a sinuosidal function for each motor.
-             wb_motor_set_position(Hexabot_Motors[i], a[i] * sin(-2.0 * M_PI * f * time + p[i]) + d[i]);
-         }
+            WalkPatt = TurnLEFT(WalkPatt, time);
          else if(COMP.degree < Gps.angle && abs(COMP.degree - Gps.angle) > 2)
-         {
-           for (i = 0; i < 9; ++i)  // Apply a sinuosidal function for each motor.
-             wb_motor_set_position(Hexabot_Motors[i], a[i] * sin(-2.0 * M_PI * f * time + p[i]) + d[i]);
-           for (i = 9; i < 18; ++i)  // Apply a sinuosidal function for each motor.
-             wb_motor_set_position(Hexabot_Motors[i], a[i] * sin(2.0 * M_PI * f * time + p[i]) + d[i]);
-         }
+            WalkPatt = TurnRIGHT(WalkPatt, time);
          else if(Gps.distance > .5)
-         {
-           for (i = 0; i < 18; ++i)  // Apply a sinuosidal function for each motor.
-             wb_motor_set_position(Hexabot_Motors[i], a[i] * sin(2.0 * M_PI * f * time + p[i]) + d[i]);
-         }
+            WalkPatt = Foward(WalkPatt, time);
        }
        else if(Gps.quadrant == 4)
        {
          Gps.angle = 90 - Gps.angle;
          if(COMP.degree > Gps.angle && abs(COMP.degree - Gps.angle) > 2)
-         {
-           for (i = 0; i < 9; ++i)  // Apply a sinuosidal function for each motor.
-             wb_motor_set_position(Hexabot_Motors[i], a[i] * sin(2.0 * M_PI * f * time + p[i]) + d[i]);
-           for (i = 9; i < 18; ++i)  // Apply a sinuosidal function for each motor.
-             wb_motor_set_position(Hexabot_Motors[i], a[i] * sin(-2.0 * M_PI * f * time + p[i]) + d[i]);
-         }
+            WalkPatt = TurnLEFT(WalkPatt, time);
          else if(COMP.degree < Gps.angle && abs(COMP.degree - Gps.angle) > 2)
-         {
-           for (i = 0; i < 9; ++i)  // Apply a sinuosidal function for each motor.
-             wb_motor_set_position(Hexabot_Motors[i], a[i] * sin(-2.0 * M_PI * f * time + p[i]) + d[i]);
-           for (i = 9; i < 18; ++i)  // Apply a sinuosidal function for each motor.
-             wb_motor_set_position(Hexabot_Motors[i], a[i] * sin(2.0 * M_PI * f * time + p[i]) + d[i]);
-         }
+            WalkPatt = TurnRIGHT(WalkPatt, time);
          else if(Gps.distance > .5)
-         {
-           for (i = 0; i < 18; ++i)  // Apply a sinuosidal function for each motor.
-             wb_motor_set_position(Hexabot_Motors[i], a[i] * sin(2.0 * M_PI * f * time + p[i]) + d[i]);
-         }
+            WalkPatt = Foward(WalkPatt, time);
        }
    };
 
@@ -281,10 +187,9 @@ int main(int argc, char **argv) {
 
   /* This is necessary to cleanup webots resources */
   COMP = Compass_Disable(COMP);
-  wb_gyro_disable(Hexabot_Gyro);
   Gps = GPS_Disable(Gps);
-  wb_keyboard_disable();
   wb_robot_cleanup();//
 
   return 0;
 }
+
