bba941b7414748ed2d174d1c115869dbde294c1a
ehalf001 <38255772+ehalf001@users.noreply.github.com>
2020-05-24 12:11:11 -0700

    Made 1 to 1 mapping
    Added 1 to 1 of internal map

--- controllers/my_controller_c/Auxillaries/PathDisplay.c
+++ controllers/my_controller_c/Auxillaries/PathDisplay.c
@@ -8,7 +8,7 @@
 #include <stdio.h>
 
 #define GROUND_X 10.0
-#define GROUND_Z 10.0
+#define GROUND_Z -10.0
 
 #define white 0xFFFFFF
 #define black 0x000000
@@ -17,11 +17,16 @@
 struct PathDisplay{
   WbDeviceTag Hexabot_PathDisplay;
   int height, width;
+
+  int color[4096];
 };
 
 struct PathDisplay PathDisplay_Init() {
   struct PathDisplay PathDisplayInit;
-
+  int i;
+  for(i = 0; i < 4096; i++)
+    PathDisplayInit.color[i] = white;
+  
   PathDisplayInit.Hexabot_PathDisplay = wb_robot_get_device("Path_Display");
   
   PathDisplayInit.width = wb_display_get_width(PathDisplayInit.Hexabot_PathDisplay);
@@ -29,6 +34,7 @@ struct PathDisplay PathDisplay_Init() {
   
   wb_display_set_color(PathDisplayInit.Hexabot_PathDisplay, white);
   wb_display_fill_rectangle(PathDisplayInit.Hexabot_PathDisplay, 0, 0, PathDisplayInit.width, PathDisplayInit.height);
+  wb_display_set_opacity(PathDisplayInit.Hexabot_PathDisplay, 0.03);
   wb_display_set_color(PathDisplayInit.Hexabot_PathDisplay, black);
   wb_display_draw_line(PathDisplayInit.Hexabot_PathDisplay, 0, PathDisplayInit.height / 2, PathDisplayInit.width - 1, PathDisplayInit.height / 2);
   wb_display_draw_text(PathDisplayInit.Hexabot_PathDisplay, "x", PathDisplayInit.width - 10, PathDisplayInit.height / 2 - 10);
@@ -41,10 +47,15 @@ struct PathDisplay PathDisplay_Init() {
 struct PathDisplay PathDisplay_Loop(struct PathDisplay PathDisplayOld, struct GPS gps) {
     struct PathDisplay PathDisplayNew = PathDisplayOld;
     // display the robot position
-    wb_display_set_opacity(PathDisplayNew.Hexabot_PathDisplay, 0.03);
+    wb_display_set_opacity(PathDisplayNew.Hexabot_PathDisplay, 0.1);
     wb_display_set_color(PathDisplayNew.Hexabot_PathDisplay, blue);
-    wb_display_fill_oval(PathDisplayNew.Hexabot_PathDisplay, PathDisplayNew.width * (gps.pos_x + GROUND_X / 2) / GROUND_X,
-                         PathDisplayNew.height * (gps.pos_z + GROUND_Z / 2) / GROUND_Z, 4, 4);
-
+    wb_display_draw_pixel(PathDisplayNew.Hexabot_PathDisplay, PathDisplayNew.width * (gps.pos_x + GROUND_X / 2) / GROUND_X,
+                         PathDisplayNew.height * (gps.pos_z + GROUND_Z / 2) / GROUND_Z);
+    
+    int coordinate = PathDisplayNew.width * (gps.pos_x + GROUND_X / 2) / GROUND_X + 64*(PathDisplayNew.height * (gps.pos_z + GROUND_Z / 2) / GROUND_Z);
+    PathDisplayNew.color[coordinate] = blue;
     return PathDisplayNew;
 }
+
+
+
